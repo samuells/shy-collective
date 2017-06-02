2b442ba175b314b39b371a21f0c8b036c53ceb1c
Samuel Snopko <samuell.snopko@gmail.com>
2017-06-02 23:47:29 +0200

    change entrance animation of home offer

--- js/home.script.js
+++ js/home.script.js
@@ -14,3 +14,33 @@ jQuery(function($) {
   'user strict';
   initHome();
 });
+
+// const initProjects = function() {
+//   $(document).foundation();
+//
+//   if (document.querySelector('.page--projects')) {
+//     var $projectsList = $('.projects-list');
+//     $projectsList.on('click', 'a:not(.choosed)', function(event) {
+//       event.preventDefault();
+//       $('a.choosed').removeClass('choosed');
+//       $(this).addClass('choosed');
+//     });
+//     if (Foundation.MediaQuery.atLeast('large')) {
+//       $projectsList.on('mouseover', 'a:not(.choosed)', function(event) {
+//         event.preventDefault();
+//         $('a.choosed').removeClass('choosed');
+//         $(this).addClass('choosed');
+//       });
+//     }
+//     $projectsList.on('click', 'a.choosed', function(event) {
+//       event.preventDefault();
+//       smoothState.load('./niro-project-v1.html');
+//     });
+//     console.log('Projects ignited');
+//   }
+// }
+//
+// jQuery(function($) {
+//   'user strict';
+//   initProjects();
+// });
--- js/scripts.all.js
+++ js/scripts.all.js
@@ -878,6 +878,36 @@ jQuery(function($) {
   initHome();
 });
 
+// const initProjects = function() {
+//   $(document).foundation();
+//
+//   if (document.querySelector('.page--projects')) {
+//     var $projectsList = $('.projects-list');
+//     $projectsList.on('click', 'a:not(.choosed)', function(event) {
+//       event.preventDefault();
+//       $('a.choosed').removeClass('choosed');
+//       $(this).addClass('choosed');
+//     });
+//     if (Foundation.MediaQuery.atLeast('large')) {
+//       $projectsList.on('mouseover', 'a:not(.choosed)', function(event) {
+//         event.preventDefault();
+//         $('a.choosed').removeClass('choosed');
+//         $(this).addClass('choosed');
+//       });
+//     }
+//     $projectsList.on('click', 'a.choosed', function(event) {
+//       event.preventDefault();
+//       smoothState.load('./niro-project-v1.html');
+//     });
+//     console.log('Projects ignited');
+//   }
+// }
+//
+// jQuery(function($) {
+//   'user strict';
+//   initProjects();
+// });
+
 const initProjects = function() {
   $(document).foundation();
 
