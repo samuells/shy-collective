0bebe2be0b7cddfc2d9714a5de01269134834d76
Samuel Snopko <samuell.snopko@gmail.com>
2017-06-18 21:37:49 +0200

    changed projects page on full screen images

--- js/projects.script.js
+++ js/projects.script.js
@@ -2,17 +2,20 @@ const initProjects = function() {
   $(document).foundation();
 
   if (document.querySelector('.page--projects')) {
+    var $thumbnailsList = $('.thumbnails-list');
     var $projectsList = $('.projects-list');
     $projectsList.on('click', 'a:not(.choosed)', function(event) {
       event.preventDefault();
       $('a.choosed').removeClass('choosed');
       $(this).addClass('choosed');
     });
-    if (Foundation.MediaQuery.atLeast('large')) {
+    if (Foundation.MediaQuery.atLeast('xlarge')) {
       $projectsList.on('mouseover', 'a:not(.choosed)', function(event) {
         event.preventDefault();
         $('a.choosed').removeClass('choosed');
         $(this).addClass('choosed');
+        $thumbnailsList.find('.showed').removeClass('showed');
+        $($thumbnailsList.find('li').get($(this).closest('li').index())).addClass('showed');
       });
     }
     $projectsList.on('click', 'a.choosed', function(event) {
