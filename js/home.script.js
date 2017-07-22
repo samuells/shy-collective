d5e6c4b33c7bb90ef336c293bbe5ab6f9cadbde5
Samuel Snopko <samuell.snopko@gmail.com>
2017-07-22 23:29:13 +0200

    added contact page and choose shaking animation

--- js/home.script.js
+++ js/home.script.js
@@ -42,6 +42,9 @@ const initHome = function() {
       $homeOffer.on('mouseleave', 'li.choosed a', function(event) {
         $(this).closest('li').removeClass('choosed');
       });
+      setTimeout(function() {
+        $('.choose').addClass('shaking');
+      }, 5200);
     }
     console.log('Home ignited');
   }
