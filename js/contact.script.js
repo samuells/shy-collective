98102389b13d28419f5dfe5b0186a91817e0e1c2
Samuel Snopko <samuell.snopko@gmail.com>
2017-07-23 10:34:09 +0200

    added js for contact

---
+++ js/contact.script.js
@@ -0,0 +1,20 @@
+const initContact = function() {
+  if (document.querySelector('.page--contact')) {
+  //   $(".js-text").each(function () {
+  //     var obj = $(this);
+  //     var html = obj.html().replace(/(\S+\s*)/g, "<span>$1</span>");
+  //     obj.html(html);
+  //   });
+  //   setTimeout(function(){
+  //     console.log('ahoj');
+  //     $('.js-text').addClass('faded');
+  //   }, 2000);
+  // }
+}
+
+
+
+jQuery(function($) {
+  'user strict';
+  initContact();
+});
