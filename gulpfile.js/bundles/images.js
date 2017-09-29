9880405471a3e474cbd0d51960ea5f825db62ffa
Samuel Snopko <samuell.snopko@gmail.com>
2017-09-29 19:33:13 +0200

    image optimized

--- gulpfile.js/bundles/images.js
+++ gulpfile.js/bundles/images.js
@@ -1,24 +1,30 @@
-// // ==================
-// // Requirements
-// // ==================
-// const gulp = require('gulp');
-// const imageOpt = require('gulp-image');
-//
-// // ==================
-// // OPTIMIZING
-// // ==================
-// gulp.task('img:opt', function() {
-//   return gulp.src('./images/**/*')
-//     .pipe(imageOpt({
-//       pngquant: true,
-//       optipng: false,
-//       zopflipng: true,
-//       jpegRecompress: false,
-//       jpegoptim: true,
-//       mozjpeg: true,
-//       gifsicle: true,
-//       svgo: true,
-//       concurrent: 10
-//     }))
-//     .pipe(gulp.dest('./assets/images'));
-// });
+// ==================
+// Requirements
+// ==================
+const gulp = require('gulp');
+const imageOpt = require('gulp-image');
+const imageResize = require('gulp-image-resize');
+
+// ==================
+// OPTIMIZING
+// ==================
+gulp.task('img:opt', function() {
+  return gulp.src('./img/original/*')
+    .pipe(imageResize({
+      width: 2000,
+      format : 'jpeg',
+      upscale : false
+    }))
+    .pipe(imageOpt({
+      pngquant: true,
+      optipng: true,
+      zopflipng: true,
+      jpegRecompress: false,
+      jpegoptim: true,
+      mozjpeg: true,
+      gifsicle: true,
+      svgo: true,
+      concurrent: 10
+    }))
+    .pipe(gulp.dest('./img/'));
+});
